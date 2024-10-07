63f80b42678f7868d188670b174eeba1399915a3
Daktic <gberg@DakticDigital.com>
2024-10-07 17:31:37 -0600

    Added checkmark svg

--- src/pages/ride_complete/rideComplete.jsx
+++ src/pages/ride_complete/rideComplete.jsx
@@ -1,30 +1,25 @@
-
+import CheckMark from "../../assets/common/check.svg";
 import "./rideComplete.css";
 
 function RideComplete () {
     return (
         <div className="box">
-            <div className="end-ride">
-                <div className="overlap">
-                    <div className="coin-group">
-                        <div className="coin-slot">
-                            <div className="slot">
-                                <div className="rectangle" />
-                                <div className="rectangle-2" />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="token-amount">
-                        <div className="overlap-2">
-                            <div className="rectangle-3" />
-                            <div className="rectangle-4" />
-                            <div className="text-wrapper">10</div>
+            <div className="overlap">
+                <img src={CheckMark} alt="checkmark" className="checkmark"/>
+                <div className="coin-group">
+                    <div className="coin-slot">
+                        <div className="slot">
+
+                            <div className="rectangle"/>
+                            <div className="rectangle-2"/>
                         </div>
                     </div>
                 </div>
+
             </div>
+
         </div>
     );
 };
 
-export { RideComplete }
+export {RideComplete}
