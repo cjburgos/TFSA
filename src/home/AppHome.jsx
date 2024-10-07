f5b9cf15a0ce861a24eeb1e12bc9f7e99bc97a9a
Daktic <gberg@DakticDigital.com>
2024-10-07 13:22:13 -0600

    Wonky file loads without iphone imports

--- src/home/AppHome.jsx
+++ src/home/AppHome.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import {ConversionRate} from "../pages/conversion_rate/ConversionRate.jsx";
-
+// import {Stake} from "../pages/stake/Stake.jsx";
 function AppHome () {
     return (
         <div>
             <ConversionRate/>
+            {/*<Stake/>*/}
         </div>
         );
 }
