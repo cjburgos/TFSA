fd8125895883d4cfd336b9540f2c702da39f1396
Carlo Relian <carlo.burgos@relianlabs.com>
2024-10-08 09:23:42 -0600

    sync fork

--- src/home/AppHome.jsx
+++ src/home/AppHome.jsx
@@ -1,10 +1,26 @@
-import React from "react";
-import { ConversionRate } from "../pages/conversion_rate/ConversionRate.jsx";
 
-export default function AppHome () {
+import {ConversionRate} from "../pages/conversion_rate/ConversionRate.jsx";
+import {Convert} from "../pages/convert/convert.jsx";
+import {RideStart} from "../pages/ride_start/rideStart.jsx";
+import {RideInProgress} from "../pages/ride_in_progress/rideInProgress.jsx";
+import {RideComplete} from "../pages/ride_complete/rideComplete.jsx";
+import {Assets} from "../pages/assets/Assets.jsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+function AppHome () {
     return (
-        <div>
-            <ConversionRate/>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<ConversionRate/>}/>
+                <Route path="/conversion_rates" element={<ConversionRate/>}/>
+                <Route path="/convert" element={<Convert/>}/>
+                <Route path="/ride_start" element={<RideStart/>}/>
+                <Route path="/ride_in_progress" element={<RideInProgress/>}/>
+                <Route path="/ride_complete" element={<RideComplete/>}/>
+                <Route path={"/assets"} element={<Assets/>}/>
+            </Routes>
+        </BrowserRouter>
         );
 }
+
+export default AppHome ;
--- src/pages/conversion_rate/ConversionRate.jsx
+++ src/pages/conversion_rate/ConversionRate.jsx
@@ -1,140 +1,27 @@
-
-import TFSACoin from "../../assets/coins/tfsa-coin.svg";
-import ellipse2 from "../../assets/pageSpecific/ConversionRate/img/ellipse-2.svg";
-import ellipse5 from "../../assets/pageSpecific/ConversionRate/img/ellipse-5.svg";
-import image1 from "../../assets/pageSpecific/ConversionRate/img/image-1.png";
-import image2 from "../../assets/pageSpecific/ConversionRate/img/image-2.png";
-import image3 from "../../assets/pageSpecific/ConversionRate/img/image-3.png";
-// import image from "../../assets/pageSpecific/ConversionRate/img/image.svg"; // missing
-import iphone16ProBlackTitaniumPortrait from "../../assets/pageSpecific/ConversionRate/img/iphone-16-pro-black-titanium-portrait.png";
-// import line12 from "../../assets/pageSpecific/ConversionRate/img/line-1-2.svg"; // missing
-// import line1 from "../../assets/pageSpecific/ConversionRate/img/line-1.svg"; // missing
-import line2 from "../../assets/pageSpecific/ConversionRate/img/line-2.svg";
+import React from "react";
 import "./conversionRate.css";
+import {RateLine} from "./RateLine.jsx";
+
+import TokenMeta from "../../../tokenMeta.json"
+import {useLocation} from "react-router-dom";
+
 
 function ConversionRate () {
-    return (
-        <div className="box">
-            <div className="conversion-rate">
-                <div className="overlap">
-                    <div className="phone">
-                        <div className="conversion-rates">
-                            <div className="overlap-group">
-                                <div className="home-indicator">
-                                    <div className="div" />
-                                </div>
-                                <div className="bezel">
-                                    <img className="iphone-pro-black" alt="Iphone pro black" src={iphone16ProBlackTitaniumPortrait} />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="rate-container">
-                        <div className="exchange-background">
-                            <div className="exchange-coin">
-                                <div className="overlap-2">
-                                    {/* <img className="line" alt="Line" src={line12} /> */}
-                                    <div className="exchange-coin-2" />
-                                    <div className="swipe-continer">
-                                        <div className="rectangle-wrapper">
-                                            <div className="rectangle" />
-                                        </div>
-                                    </div>
-                                    <div className="rate">
-                                        <div className="overlap-3">
-                                            <div className="rectangle-2" />
-                                            <div className="text-wrapper">0.79</div>
-                                        </div>
-                                    </div>
-                                    <div className="MTA-coin">
-                                        <div className="overlap-4">
-                                            <div className="ellipse" />
-                                            <div className="ellipse-2" />
-                                            <img className="image" alt="Image" src={image3} />
-                                            <img className="img" alt="Ellipse" src={ellipse2} />
-                                            <img className="ellipse-3" alt="Ellipse" src={ellipse5} />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="overlap-wrapper">
-                                <div className="overlap-2">
-                                    {/* <img className="line" alt="Line" src={image} /> */}
-                                    <div className="exchange-coin-2" />
-                                    <div className="overlap-group-wrapper">
-                                        <div className="div-wrapper">
-                                            <div className="rectangle" />
-                                        </div>
-                                    </div>
-                                    <div className="rate">
-                                        <div className="overlap-3">
-                                            <div className="rectangle-2" />
-                                            <div className="rate-2">0.84</div>
-                                        </div>
-                                    </div>
-                                    <div className="muni-coin">
-                                        <div className="overlap-5">
-                                            <div className="metro-coin">
-                                                <div className="ellipse-wrapper">
-                                                    <div className="ellipse-4" />
-                                                </div>
-                                            </div>
-                                            <img className="image-2" alt="Image" src={image2} />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="exchange-coin-3">
-                                <div className="overlap-2">
-                                    {/* <img className="line" alt="Line" src={line1} /> */}
-                                    <div className="exchange-coin-2" />
-                                    <div className="swipe-continer-2">
-                                        <div className="overlap-group-2">
-                                            <div className="rectangle-3" />
-                                            <div className="rectangle-4" />
-                                            <div className="swipe-background" />
-                                        </div>
-                                    </div>
-                                    <div className="rate">
-                                        <div className="overlap-3">
-                                            <div className="rectangle-2" />
-                                            <div className="text-wrapper-2">1.24</div>
-                                        </div>
-                                    </div>
-                                    <div className="metro-coin-2">
-                                        <div className="overlap-5">
-                                            <div className="metro-coin">
-                                                <div className="overlap-group-3">
-                                                    <div className="ellipse-5" />
-                                                </div>
-                                            </div>
-                                            <img className="image-3" alt="Image" src={image1} />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <img className="line-2" alt="Line" src={line2} />
-                        </div>
-                    </div>
-                    <div className="title">
-                        <div className="overlap-6">
-                            <div className="rectangle-5" />
-                            <div className="text-wrapper-3">Conversion Rate</div>
-                        </div>
-                    </div>
-                    <div className="TFSA-container">
-                        <div className="overlap-7">
-                            <div className="swipe-continer-3">
-                                <div className="rectangle-6" />
-                            </div>
-                            <img className="TFSA-coin" alt="Tfsa coin" src={TFSACoin} />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-};
+    const location = useLocation();
+    const {token, amountAvailable} = location.state;
+    if (!token || !amountAvailable) {
+        return <div>Error: Missing token or amount available data.</div>;
+    }
 
+    const filteredItems = Object.values(TokenMeta).filter(item => item.shortName !== token.shortName);
+
+    return (
+    <div className={"rate-outer"}>
+        {filteredItems.map(item => (
+            <RateLine balanceToken={token} token={item}  balanceAvailable={amountAvailable} amountAvailable={amountAvailable} key={item.shortName}/>
+        ))}
+    </div>
+  );
+};
 
 export { ConversionRate };
