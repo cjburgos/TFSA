bfff9a042f53253deebedc656e1da4d6d8569188
Daktic <gberg@DakticDigital.com>
2024-10-07 22:19:44 -0600

    added navigation click on line

--- src/pages/assets/AssetLine.jsx
+++ src/pages/assets/AssetLine.jsx
@@ -1,9 +1,19 @@
 import {getCoinName} from "../../lib.js";
 import "./assets.css"
+import {useNavigate} from "react-router-dom";
 
 function AssetLine(props) {
+
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate("/conversion_rates", {state: {
+                coin: props.token,
+                rate: props.value
+            }});
+    }
     return (
-        <div className={"asset-line"}>
+        <div className={"asset-line"} onClick={handleClick}>
             <img src={props.token} className={"asset-line-item"} alt={"Token"}/>
             <p className={"asset-line-item"}>{getCoinName(props.token)}</p>
             <p className={"asset-line-item"}>{props.value}</p>
