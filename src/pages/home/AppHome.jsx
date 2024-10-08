13469632f5ce6b2108c636a65e210127da385e80
Carlo Relian <carlo.burgos@relianlabs.com>
2024-10-08 10:09:37 -0600

    add navbar and signout

--- src/pages/home/AppHome.jsx
+++ src/pages/home/AppHome.jsx
@@ -5,22 +5,24 @@ import {RideStart} from "../ride_start/rideStart.jsx";
 import {RideInProgress} from "../ride_in_progress/rideInProgress.jsx";
 import {RideComplete} from "../ride_complete/rideComplete.jsx";
 import {Assets} from "../assets/Assets.jsx";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
+import {NavBar} from "../../components/common/NavBar.jsx";
 
 function AppHome () {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Assets/>}/>
-                <Route path="/conversion_rates" element={<ConversionRate/>}/>
-                <Route path="/convert" element={<Convert/>}/>
-                <Route path="/ride_start" element={<RideStart/>}/>
-                <Route path="/ride_in_progress" element={<RideInProgress/>}/>
-                <Route path="/ride_complete" element={<RideComplete/>}/>
-                {/* <Route path={"/assets"} element={<Assets/>}/> */}
-            </Routes>
-        </BrowserRouter>
-        );
+        <div>
+        <NavBar/>
+        <Routes>
+            <Route path="/" element={<Assets/>}/>
+            <Route path="/conversion_rates" element={<ConversionRate/>}/>
+            <Route path="/convert" element={<Convert/>}/>
+            <Route path="/ride_start" element={<RideStart/>}/>
+            <Route path="/ride_in_progress" element={<RideInProgress/>}/>
+            <Route path="/ride_complete" element={<RideComplete/>}/>
+            {/* <Route path={"/assets"} element={<Assets/>}/> */}
+        </Routes>
+        </div>
+    );
 }
 
 export default AppHome ;
